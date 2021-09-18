33cf6abf39e8f53016a0d1d78153e8c1699286ec
jimmyspann <jimmyleespann@outlook.com>
2021-09-18 16:31:48 -0400

    add custom data

---
+++ app.js
@@ -0,0 +1,254 @@
+const data = [ 
+    {
+        "id": 128765120,
+        "firstname": "Letizia",
+        "lastname": "Alva",
+        "city": "Oranjestad",
+        "email": "Letizia.Alva@gmail.com",
+        "dependencies": [
+            {
+            "id": 9479659711,
+            "firstname": "Jaclyn",
+            "lastname": "Thar",
+            "city": "Iloilo City",
+            "email": "Jaclyn.Thar@gmail.com",
+            "dependencies": []
+            },
+            {
+                "id": 7124552279,
+                "firstname": "Agathe",
+                "lastname": "Erskine",
+                "city": "Jakarta",
+                "email": "Agathe.Erskine@gmail.com",
+                "dependencies": []
+            }
+        ]
+    },
+    {
+        "id": 2597431915,
+        "firstname": "Cristabel",
+        "lastname": "Suanne",
+        "city": "Guadalajara",
+        "email": "Cristabel.Suanne@gmail.com",
+        "dependencies": [
+            {
+                "id": 9386277520,
+                "firstname": "Almeta",
+                "lastname": "Arne",
+                "city": "Abuja",
+                "email": "Almeta.Arne@gmail.com",
+                "dependencies": []
+            },
+            {
+                "id": 3054745669,
+                "firstname": "Aryn",
+                "lastname": "Moseley",
+                "city": "Antwerp",
+                "email": "Aryn.Moseley@gmail.com",
+                "dependencies": [{
+                    "id": 8576311021,
+                    "firstname": "Justinn",
+                    "lastname": "Corabella",
+                    "city": "Auckland",
+                    "email": "Justinn.Corabella@gmail.com",
+                    "dependencies": [
+                        {
+                            "id": 1296560725,
+                            "firstname": "Lexine",
+                            "lastname": "Junie",
+                            "city": "Johor Bahru",
+                            "email": "Lexine.Junie@gmail.com",
+                            "dependencies": []
+                        },
+                    ]
+                },]
+            },
+        ]
+    },
+    {
+        "id": 2120484163,
+        "firstname": "Cassandra",
+        "lastname": "Burch",
+        "city": "Cologne",
+        "email": "Cassandra.Burch@gmail.com",
+        "dependencies": []
+    },
+    {
+        "id": 9780067885,
+        "firstname": "Gale",
+        "lastname": "Corrine",
+        "city": "Macapá",
+        "email": "Gale.Corrine@gmail.com",
+        "dependencies": [
+            {
+            "id": 8770061600,
+            "firstname": "Nanete",
+            "lastname": "Dituri",
+            "city": "Abidjan",
+            "email": "Nanete.Dituri@gmail.com",
+            "dependencies": [
+                {
+                    "id": 9884622740,
+                    "firstname": "Jaclyn",
+                    "lastname": "Lesley",
+                    "city": "Philadelphia",
+                    "email": "Jaclyn.Lesley@gmail.com",
+                    "dependencies": []
+                },
+            ]
+        },
+    ]
+    },
+    {
+        "id": 8381981929,
+        "firstname": "Steffane",
+        "lastname": "Salvidor",
+        "city": "Fort-de-France",
+        "email": "Steffane.Salvidor@gmail.com",
+        "dependencies": [
+            {
+                "id": 2914280722,
+                "firstname": "Willetta",
+                "lastname": "Yate",
+                "city": "Nicosia",
+                "email": "Willetta.Yate@gmail.com",
+                "dependencies": [
+                    {
+                        "id": 98046676,
+                        "firstname": "Jeanna",
+                        "lastname": "Gaynor",
+                        "city": "Valparaíso",
+                        "email": "Jeanna.Gaynor@gmail.com",
+                        "dependencies": [
+                            {
+                                "id": 7316686264,
+                                "firstname": "Korrie",
+                                "lastname": "Han",
+                                "city": "Banda Aceh",
+                                "email": "Korrie.Han@gmail.com",
+                                "dependencies": [
+                                    {
+                                        "id": 9333308746,
+                                        "firstname": "Vere",
+                                        "lastname": "Lewes",
+                                        "city": "Dubai",
+                                        "email": "Vere.Lewes@gmail.com",
+                                        "dependencies": []
+                                    },
+                                ]
+                            },
+                        ]
+                    },
+                ]
+            },
+        ]
+    }, 
+    {
+        "id": 2391022385,
+        "firstname": "Keelia",
+        "lastname": "Crudden",
+        "city": "Vienna",
+        "email": "Keelia.Crudden@gmail.com",
+        "dependencies": [
+            {
+                "id": 4140525236,
+                "firstname": "Deedee",
+                "lastname": "Narton",
+                "city": "Madrid",
+                "email": "Deedee.Narton@gmail.com",
+                "dependencies": []
+            },
+            {
+                "id": 350903143,
+                "firstname": "Elvira",
+                "lastname": "Raffo",
+                "city": "Chiang Mai",
+                "email": "Elvira.Raffo@gmail.com",
+                "dependencies": []
+            },
+            {
+                "id": 3952412370,
+                "firstname": "Aili",
+                "lastname": "Kenwood",
+                "city": "Srinagar",
+                "email": "Aili.Kenwood@gmail.com",
+                "dependencies": []
+            },
+            {
+                "id": 3654858687,
+                "firstname": "Flory",
+                "lastname": "Burnside",
+                "city": "Graz",
+                "email": "Flory.Burnside@gmail.com",
+                "dependencies": []
+            },
+        ]
+    },
+    {
+        "id": 5417356844,
+        "firstname": "Chandra",
+        "lastname": "Gaynor",
+        "city": "Bucharest",
+        "email": "Chandra.Gaynor@gmail.com",
+        "dependencies": [
+            {
+                "id": 3472358293,
+                "firstname": "Jessamyn",
+                "lastname": "Uird",
+                "city": "Gaza",
+                "email": "Jessamyn.Uird@gmail.com",
+                "dependencies": [
+                    {
+                        "id": 6165251503,
+                        "firstname": "Leanna",
+                        "lastname": "Douglass",
+                        "city": "Kraków",
+                        "email": "Leanna.Douglass@gmail.com",
+                        "dependencies": []
+                    },
+                ]
+            },
+            {
+                "id": 1909859396,
+                "firstname": "Anica",
+                "lastname": "Weinreb",
+                "city": "Denpasar",
+                "email": "Anica.Weinreb@gmail.com",
+                "dependencies": []
+            },
+        ]
+    },    
+    {
+        "id": 5819233310,
+        "firstname": "Nelle",
+        "lastname": "Erminia",
+        "city": "Saipan",
+        "email": "Nelle.Erminia@gmail.com",
+        "dependencies": [
+            {
+                "id": 4711591350,
+                "firstname": "Judy",
+                "lastname": "Janene",
+                "city": "Tashkent",
+                "email": "Judy.Janene@gmail.com",
+                "dependencies": []
+            },
+            {
+                "id": 6439533613,
+                "firstname": "Damaris",
+                "lastname": "Lewes",
+                "city": "Samara",
+                "email": "Damaris.Lewes@gmail.com",
+                "dependencies": []
+            },
+        ]
+    }, 
+    {
+        "id": 4273599996,
+        "firstname": "Nonnah",
+        "lastname": "Sidonius",
+        "city": "Tashkent",
+        "email": "Nonnah.Sidonius@gmail.com",
+        "dependencies": []
+    }
+];
