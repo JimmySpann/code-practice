83c70f6700da9c72d5e2863869f240a03152c115
jimmyspann <jimmyleespann@outlook.com>
2021-09-19 16:59:07 -0400

    update to HTML class. Add description

--- class.js
+++ class.js
@@ -1,5 +1,14 @@
 class HTML {
-    static createElement(type, attributes) { // add : HTMLElement if decide to use node
+    /**
+    * Shortcut for creating html elements. All attributes can be add in the attributes parameter.
+    *
+    * @param {String}   type Type of element in string format.
+    *
+    * @param {Object}   attributes Object containing any attributes. Ex: { class: "my-class", id="123"}
+    * 
+    * @return {HTMLElement} Returns created HTMLElement
+    */
+    static createElement(type, attributes) {
         const newElement = document.createElement(type)
         for(const key in attributes)
         {
