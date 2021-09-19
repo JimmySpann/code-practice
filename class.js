6b551b1480ddc5a8ab63d138a3dd64c8b87259d8
jimmyspann <jimmyleespann@outlook.com>
2021-09-19 18:59:31 -0400

    add proper documentation

--- class.js
+++ class.js
@@ -1,6 +1,6 @@
 class HTML {
     /**
-    * Shortcut for creating html elements. All attributes can be add in the attributes parameter.
+    * Shortcut for creating html elements. All attributes can be added in the attributes parameter.
     *
     * @param {String}   type Type of element in string format.
     *
@@ -25,6 +25,19 @@ class HTML {
         return newElement
     }
 
+    /**
+    * Shortcut for creating multiple html elements at once. All attributes can be added in the attributes parameter.
+    *
+    * @param {String}   type Type of element in string format.
+    *
+    * @param {Number}   amount Number of elements to create.
+    * 
+    * @param {Object}   similarAttributes Object containing any attributes all elements will share. Ex: { class: "my-class", id="123"} Will take textContent and innerHTML
+    * 
+    * @param {Object}   [differentAttributes={}] An object that contains attributes particular to some elements. The object keys coincide with the array number and the object for the key is the property. Ex: { 0: { class: "first-class" }, 2: { class: "3rd-class" } } - 2nd element will only have attributes from similarAttributes
+    * 
+    * @return {Array<HTMLElement>} Returns created an array of HTMLElement
+    */
     static createSimilarElements(type, amount, similarAttributes, differentAttributes = {}) {
         const elements = [];
         for(let i = 0; i < amount; i++)
